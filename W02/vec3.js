defc6641250df4e051eac206317f8860bc8ab8c8
github229x109x <github229x109x@stu.kobe-u.ac.jp>
2022-04-13 15:17:26 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -22,4 +22,41 @@ class Vec3
         this.y -=v.y;
         this.z -=v.z;
     }
+
+    sum()
+    {
+        return this.x + this.y + this.z;
+    }
+
+    min()
+    {
+        const m =  this.x < this.y ? this.x : this.y;
+        return m < this.z ? m : this.z;
+    }
+
+    max()
+    {
+        const m = this.x > this.y ? this.x : this.y;
+        return m > this.z ? this.z : m;}
+    }
+
+    mid()
+    {
+        return this.sum() - this.min() -this.max();
+    }
+
+    cross( v )
+    {
+        var x = this.x, y = this.y, z = this.z;
+        this.x = y * v.z - z * v.y;
+        this.y = z * v.x - x * v.z;
+        this.z = x * v.y - y * v.x;
+        return this;
+    }
+
+    length()
+    {
+        return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+    }
+    
 }
